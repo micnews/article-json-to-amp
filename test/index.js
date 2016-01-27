e064a6b8574ced2ee084d0fd2ea1a4bf827a6ca9
Serge <iefserge@runtimejs.org>
2016-01-27 09:08:22 +0200

    Support linebreaks, image and youtube embeds

--- test/index.js
+++ test/index.js
@@ -2,11 +2,12 @@ import test from 'ava';
 import tsml from 'tsml';
 import toAmp from '../dist';
 
-test('first', t => {
+test('paragraphs', t => {
   const data = [{
     type: 'paragraph',
     children: [
       { type: 'text', href: 'http://mic.com', content: 'link' },
+      { type: 'linebreak' },
       { type: 'text', content: 'normal text' },
       { type: 'text', bold: true, content: 'bold text' },
       { type: 'text', italic: true, content: 'italic text' }
@@ -22,9 +23,41 @@ test('first', t => {
     `<article>
       <p>
         <a href="http://mic.com">link</a>
+        <br></br>
         normal text<b>bold text</b><i>italic text</i>
       </p>
       <p>other text</p>
+    </article>`
+  );
+});
+
+test('embeds', t => {
+  const data = [{
+    type: 'paragraph',
+    children: [
+      { type: 'text', content: 'normal text' }
+    ]
+  }, {
+    type: 'embed',
+    embedType: 'youtube',
+    youtubeId: 'abc'
+  }, {
+    type: 'embed',
+    embedType: 'image',
+    src: 'http://example.com/image.jpg',
+    width: 600,
+    height: 200
+  }];
+
+  t.is(toAmp(data), tsml
+    `<article>
+      <p>normal text</p>
+      <figure>
+        <amp-youtube data-videoid="abc" layout="responsive" width="480" height="270"></amp-youtube>
+      </figure>
+      <figure>
+        <amp-img width="600" height="200" layout="responsive" src="http://example.com/image.jpg"></amp-img>
+      </figure>
     </article>`
   );
 });
