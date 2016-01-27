a88616b55f56743d566ebfbafc660e68c64e5d27
Serge <iefserge@runtimejs.org>
2016-01-27 09:34:54 +0200

    Add example

---
+++ example.js
@@ -0,0 +1,31 @@
+const convertToAmp = require('./');
+const article = [
+  {
+    type: 'paragraph',
+    children: [
+      {
+        type: 'text',
+        content: 'This is the text and '
+      },
+      {
+        type: 'text',
+        bold: true,
+        content: 'some bold text '
+      },
+      {
+        type: 'text',
+        href: 'http://example.com',
+        content: 'some link'
+      }
+    ]
+  },
+  {
+    type: 'embed',
+    embedType: 'image',
+    src: 'http://example/image.jpg',
+    width: 300,
+    height: 150
+  }
+];
+
+console.log(convertToAmp(article));
