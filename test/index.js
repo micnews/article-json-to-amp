e2066b86c86f4d1f615ffee3bc7234b334e7ace3
Serge <iefserge@runtimejs.org>
2016-01-31 01:38:54 +0200

    Iframe and blockquote tests

--- test/index.js
+++ test/index.js
@@ -23,7 +23,7 @@ test('paragraphs', t => {
     `<article>
       <p>
         <a href="http://mic.com">link</a>
-        <br></br>
+        <br/>
         normal text<b>bold text</b><i>italic text</i>
       </p>
       <p>other text</p>
@@ -84,13 +84,60 @@ test('image with caption', t => {
     }]
   }];
 
-  console.log(toAmp(data));
-
   t.is(toAmp(data), tsml
     `<article>
       <figure>
         <amp-img width="600" height="200" layout="responsive" src="http://example.com/image.jpg"></amp-img>
         <figcaption>Source: <a href="http://example.com/author">Author</a></figcaption>
+      </figure>
+    </article>`
+  );
+});
+
+test('blockquote', t => {
+  const data = [{
+    type: 'blockquote',
+    children: [{
+      type: 'paragraph',
+      children: [{
+        type: 'text',
+        content: 'abc'
+      }]
+    }, {
+      type: 'paragraph',
+      children: [{
+        type: 'text',
+        content: 'def',
+        bold: true
+      }]
+    }]
+  }];
+
+  t.is(toAmp(data), tsml
+    `<article>
+      <blockquote>
+        <p>abc</p>
+        <p><b>def</b></p>
+      </blockquote>
+    </article>`
+  );
+});
+
+test('custom secure iframe', t => {
+  const data = [{
+    type: 'embed',
+    embedType: 'custom',
+    src: 'https://example.com/frame',
+    width: 600,
+    height: 200,
+    secure: true,
+    caption: []
+  }];
+
+  t.is(toAmp(data), tsml
+    `<article>
+      <figure>
+        <amp-iframe width="600" height="200" layout="responsive" frameborder="0" src="https://example.com/frame"></amp-iframe>
       </figure>
     </article>`
   );
